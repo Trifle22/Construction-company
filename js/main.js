e94cad0515221e519769618003d74e83f6ca954f
Trifle22 <xromchenkov99@gmail.com>
2020-06-27 16:29:23 +0300

    + sidebar + script

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $('.menu-btn').on('click', function(e) {
     e.preventDefault;
     $(this).toggleClass('menu-btn_active');
+    $('.hamburger-menu').toggleClass('hamburger-menu-active');
+
 });
+$('.close-menu-btn').on('click', function(e) {
+    e.preventDefault;
+    $('.hamburger-menu').removeClass('hamburger-menu-active');
+    $('.menu-btn').removeClass('menu-btn_active');
+
+})
 
 $('.dropdown').on('click', function(e) {
     e.preventDefault;
